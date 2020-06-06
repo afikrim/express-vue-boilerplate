a51f41f018b8a10a3e6eee465cbd77280715d10d
Aziz Fikri <afikrim10@student.ub.ac.id>
2020-06-07 04:34:04 +0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb-base"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    "no-debug": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
